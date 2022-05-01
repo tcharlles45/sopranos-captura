760bc4164efb4caf0a52e881f02441163e938dac
Tcharlles <tcharlles3@gmail.com>
2022-05-01 20:51:52 -0300

    final 1.0

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+/*CARDS*/ 
+
 var card = document.querySelector('.card');
 card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
 });
+
+var card2 = document.querySelector('.card2');
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+
+var card3 = document.querySelector('.card3');
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+
+/////
+
 
